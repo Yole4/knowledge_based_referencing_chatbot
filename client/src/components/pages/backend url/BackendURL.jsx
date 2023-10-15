799dc7943be259a2e03f0c23a241726ef9056b23
Eloy <shelomora60@gmail.com>
2023-10-15 21:48:22 +0800

    front

--- client/src/components/pages/backend url/BackendURL.jsx
+++ client/src/components/pages/backend url/BackendURL.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 function BackendURL() {
-  const backendUrl = 'http://localhost:3001';
+  // const backendUrl = 'http://localhost:3001';
+  const backendUrl = "https://tcas-chatbot-backend.onrender.com";
   return backendUrl;
 }
 
