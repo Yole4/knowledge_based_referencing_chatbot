f546e5ac4e2203dee8c5d34937c780010b9d86c2
Eloy <shelomora60@gmail.com>
2023-10-21 01:52:14 +0800

    request not

--- server/utils/scan document/ScanDocument.js
+++ server/utils/scan document/ScanDocument.js
@@ -1,40 +1,40 @@
-const pdfjsLib = require('pdfjs-dist');
+// const pdfjsLib = require('pdfjs-dist');
 
-function processFile(pdfPath) {
-  return pdfjsLib.getDocument(pdfPath).promise
-    .then(pdfDoc => {
-      const numPages = pdfDoc.numPages;
-      const pageTextPromises = [];
+// function processFile(pdfPath) {
+//   return pdfjsLib.getDocument(pdfPath).promise
+//     .then(pdfDoc => {
+//       const numPages = pdfDoc.numPages;
+//       const pageTextPromises = [];
 
-      function extractTextFromPage(pageNum) {
-        return pdfDoc.getPage(pageNum)
-          .then(page => {
-            return page.getTextContent()
-              .then(textContent => {
-                const textItems = textContent.items;
-                let pageText = '';
+//       function extractTextFromPage(pageNum) {
+//         return pdfDoc.getPage(pageNum)
+//           .then(page => {
+//             return page.getTextContent()
+//               .then(textContent => {
+//                 const textItems = textContent.items;
+//                 let pageText = '';
 
-                for (let i = 0; i < textItems.length; i++) {
-                  pageText += textItems[i].str + ' ';
-                }
+//                 for (let i = 0; i < textItems.length; i++) {
+//                   pageText += textItems[i].str + ' ';
+//                 }
 
-                return pageText;
-              });
-          });
-      }
+//                 return pageText;
+//               });
+//           });
+//       }
 
-      for (let pageNum = 1; pageNum <= numPages; pageNum++) {
-        pageTextPromises.push(extractTextFromPage(pageNum));
-      }
+//       for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+//         pageTextPromises.push(extractTextFromPage(pageNum));
+//       }
 
-      return Promise.all(pageTextPromises)
-        .then(pageTexts => {
-          return pageTexts;
-        });
-    })
-    .catch(error => {
-      console.error('Error loading PDF document', error);
-    });
-}
+//       return Promise.all(pageTextPromises)
+//         .then(pageTexts => {
+//           return pageTexts;
+//         });
+//     })
+//     .catch(error => {
+//       console.error('Error loading PDF document', error);
+//     });
+// }
 
-module.exports = {processFile}; 
+// module.exports = {processFile}; 
